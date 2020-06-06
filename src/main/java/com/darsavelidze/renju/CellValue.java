3d470a142a9a56c4051e69b7e96c6af81d94de57
darsavelli <darsavelli19@gmail.com>
2020-06-06 19:55:04 +0300

    Added class CellValue

---
+++ src/main/java/com/darsavelidze/renju/CellValue.java
@@ -0,0 +1,15 @@
+package com.darsavelidze.renju;
+
+public enum CellValue {
+	EMPTY(' '), HUMAN('X'), COMPUTER('O');
+
+	private char value;
+
+	private CellValue(char value) {
+		this.value = value;
+	}
+
+	public String getValue() {
+		return String.valueOf(value);
+	}
+}
